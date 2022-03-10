712cb7adc2bb0ad26be6740bf8403854fd533f6e
Filipe Soares <filipecarlossoares@gmail.com>
2022-03-10 18:02:37 -0300

    add bootstrap to client

---
+++ client/pages/_app.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default AppComponent;
