6c61395e5aeff0b413909c8afd4bec4eff291cd0
Filipe Soares <filipecarlossoares@gmail.com>
2022-05-01 10:42:33 -0300

    add id on tests that was failling

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -6,6 +6,7 @@ import { Ticket } from '../../models/ticket';
 it('fetches the order', async () => {
   // Create a ticket
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
@@ -32,6 +33,7 @@ it('fetches the order', async () => {
 it('returns an error if one user tries to fetch another users order', async () => {
   // Create a ticket
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
